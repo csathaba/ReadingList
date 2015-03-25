7f23fe4f327b3bdc2cab6a6ae04fc2e6a3e585c1
Csaba Thamo <csabathamo86@gmail.com>
2015-03-25 07:33:41 +0100

    Initial commit

---
+++ app/models/book.rb
@@ -0,0 +1,32 @@
+class Book < ActiveRecord::Base
+	has_many :book_genres
+	has_many :genres, through: :book_genres
+
+	# scope :finished, ->{ where('finished_on not null')}
+	scope :finished, ->{ where.not(finished_on: nil)}
+	scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
+	scope :search, ->(keyword){ where('keywords like ?', "%#{keyword.downcase}%") if keyword.present?}
+	# scope :filter, ->(genre_id){ where(joins(:book_genres).where('book_genres.genre_id = ?' , genre_id))}
+
+	scope :filter, ->(name){
+   		joins(:genres).where('genres.name = ?', name) if name.present?
+ 	}
+
+	before_save :set_keywords
+	# def self.search(keyword)
+	# 	if keyword.present?
+	# 		where(title: keyword)
+	# 	else 
+	# 		all
+	# 	end
+	# end
+	def finished?
+		finished_on.present?
+	end
+
+	protected
+		def set_keywords
+			self.keywords = [title, author, description].map { |p| p.downcase}.join(' ')
+			# .(&:downcase)
+		end
+end
---
+++ db/migrate/20150302194439_add_set_keywords_to_books.rb
@@ -0,0 +1,5 @@
+class AddSetKeywordsToBooks < ActiveRecord::Migration
+  def change
+    add_column :books, :keywords, :text
+  end
+end
