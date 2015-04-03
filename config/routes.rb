b333133401c1af39770a62851f8fbbdaabc88e7d
Csaba Thamo <csabathamo86@gmail.com>
2015-04-03 10:09:05 +0200

    User Controller and Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+
   resources :books
   root to: 'books#index'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303065549) do
+ActiveRecord::Schema.define(version: 20150403074045) do
 
   create_table "book_genres", force: true do |t|
     t.integer  "book_id"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20150303065549) do
 
   create_table "genres", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
